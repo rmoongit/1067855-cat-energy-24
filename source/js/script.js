407f2fa850a629354a15913ea0f5fe62e1f29df3
rmoongit <emil_tt@mail.ru>
2021-12-12 19:11:01 +0300

    index style 1.1

--- source/js/script.js
+++ source/js/script.js
@@ -6,9 +6,13 @@ navMain.classList.remove('main-nav--nojs');
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('nav__wrapper--closed')) {
     navMain.classList.remove('nav__wrapper--closed');
-    navMain.classList.add('nav__wrapper--opened');
-  } else {
+    navToggle.classList.remove('burger--closed');
+    navToggle.classList.add('burger--opened');
+  }
+
+  else {
     navMain.classList.add('nav__wrapper--closed');
-    navMain.classList.remove('nav__wrapper--opened');
+    navToggle.classList.remove('burger--opened');
+    navToggle.classList.add('burger--closed');
   }
 });
