762d003eee23b2a70762e95ff51df775f1b1f18e
rmoongit <emil_tt@mail.ru>
2021-12-15 19:57:36 +0300

    index style 1.2 map

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,5 @@
-var navMain = document.querySelector('.nav__wrapper');
-var navToggle = document.querySelector('.burger');
+let navMain = document.querySelector('.nav__wrapper');
+let navToggle = document.querySelector('.burger');
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -16,3 +16,21 @@ navToggle.addEventListener('click', function () {
     navToggle.classList.add('burger--closed');
   }
 });
+
+
+// map
+let map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 59.938724585789195, lng: 30.323048020098348},
+    zoom: 9
+  });
+
+  let marker = new google.maps.Marker({
+    position: {lat: 59.938724585789195, lng: 30.323048020098348},
+    map: map,
+    title: 'hello',
+    icon: 'map-pin.png',
+    draggable: false
+  });
+}
