80ed4c20818ac4a315ffaf1115d4a70bb7f14738
rmoongit <emil_tt@mail.ru>
2021-12-16 23:52:20 +0300

    form style 1.1

--- source/js/script.js
+++ source/js/script.js
@@ -4,19 +4,9 @@ let navToggle = document.querySelector('.burger');
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('nav__wrapper--closed')) {
-    navMain.classList.remove('nav__wrapper--closed');
-    navToggle.classList.remove('burger--closed');
-    navToggle.classList.add('burger--opened');
-  }
-
-  else {
-    navMain.classList.add('nav__wrapper--closed');
-    navToggle.classList.remove('burger--opened');
-    navToggle.classList.add('burger--closed');
-  }
+  navMain.classList.toggle('nav__wrapper--closed');
+  navToggle.classList.toggle('burger--closed');
 });
-
 
 // map
 let map;
