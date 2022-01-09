b5feefb4db6eb3869f34f1d42cfdfc4434d1f02b
rmoongit <emil_tt@mail.ru>
2022-01-09 23:02:39 +0300

    исправил замечание. исправил webp, svg, input.

--- source/js/script.js
+++ source/js/script.js
@@ -1,16 +1,19 @@
 let navMain = document.querySelector('.nav__wrapper');
 let navToggle = document.querySelector('.burger');
+let pageMain = document.querySelector('.no-js');
 
-navMain.classList.remove('nav__wrapper--nojs');
-navToggle.classList.remove('burger--nojs');
+// navMain.classList.remove('no-js--nav');
+// navToggle.classList.remove('no-js');
+pageMain.classList.remove('no-js');
 
 navToggle.addEventListener('click', function () {
-  navMain.classList.toggle('nav__wrapper--closed');
-  navToggle.classList.toggle('burger--closed');
+  navMain.classList.toggle('nav__wrapper--opened');
+  navToggle.classList.toggle('burger--opened');
 });
 
 // map
 let map;
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 59.938724585789195, lng: 30.323048020098348},
