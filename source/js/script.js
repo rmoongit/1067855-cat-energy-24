6ad95017e19a8ffdcabde9910e3a3d814cf4932c
rmoongit <emil_tt@mail.ru>
2022-01-10 21:15:17 +0300

    исправил замечание fonts, del js map.

--- source/js/script.js
+++ source/js/script.js
@@ -2,8 +2,6 @@ let navMain = document.querySelector('.nav__wrapper');
 let navToggle = document.querySelector('.burger');
 let pageMain = document.querySelector('.no-js');
 
-// navMain.classList.remove('no-js--nav');
-// navToggle.classList.remove('no-js');
 pageMain.classList.remove('no-js');
 
 navToggle.addEventListener('click', function () {
@@ -12,20 +10,3 @@ navToggle.addEventListener('click', function () {
 });
 
 // map
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 59.938724585789195, lng: 30.323048020098348},
-    zoom: 9
-  });
-
-  let marker = new google.maps.Marker({
-    position: {lat: 59.938724585789195, lng: 30.323048020098348},
-    map: map,
-    title: 'hello',
-    icon: 'map-pin.png',
-    draggable: false
-  });
-  initMap();
-}
